8b56297a011a3884bab058d5a48c05125805151a
LENOVO <627107345@qq.com>
2020-09-23 20:21:14 +0800

    这咋

--- app/src/main/java/com/lchj/meet/ui/MainActivity.java
+++ app/src/main/java/com/lchj/meet/ui/MainActivity.java
@@ -10,18 +10,12 @@ import android.util.Log;
 import android.view.View;
 
 import com.lchj.meet.R;
-import com.lchj.meet.model.User;
-import com.lchj.meet.utils.LiuUtils;
-import com.lchj.meet.widgets.TouchPictureView;
 
 public class MainActivity extends BaseActivity {
-    private TouchPictureView mTouchPictureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mTouchPictureView = findViewById(R.id.mTouchPictureView);
-        mTouchPictureView.setOnViewResultListener(() -> LiuUtils.makeText(MainActivity.this, "验证成功"));
     }
 
     @Override
